eada25443ad0ae08943a3436d533e13f40d74396
ThiwankaLakshan <thiwankalakshan007@gmail.com>
2026-09-02 11:41:11 -0400

    Fix Python linting issues

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,5 @@
-# Uncomment the following imports before adding the Model code
-
-from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 
 class CarMake(models.Model):
@@ -14,7 +11,10 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )
     dealer_id = models.IntegerField(null=True, blank=True)
     name = models.CharField(max_length=100)
 
